2325ff6b2c66281023308704203b67adad970b64
unknown <biplob2358@gmail.com>
2022-08-16 19:05:43 +0600

    done

--- js/home.js
+++ js/home.js
@@ -3,6 +3,10 @@ document.getElementById("deposite-btn").addEventListener("click", function () {
   const depositeElement = document.getElementById("input-deposite");
   const newDepositeTotalString = depositeElement.value;
   const newDeposite = parseFloat(newDepositeTotalString);
+  if (isNaN(newDeposite)) {
+    alert("Please provide a valid ammount");
+    return;
+  }
 
   //previousDeposite
   const previousDepositeElement = document.getElementById("deposite");
@@ -21,4 +25,42 @@ document.getElementById("deposite-btn").addEventListener("click", function () {
   balanceElement.innerText = currentBalanceTotal;
 
   depositeElement.value = "";
+});
+
+//withdraw
+
+document.getElementById("withdraw-btn").addEventListener("click", function () {
+  const newWithdrawElement = document.getElementById("input-withdraw");
+  const newWithdrawTotalString = newWithdrawElement.value;
+  const newWithdrawTotal = parseFloat(newWithdrawTotalString);
+
+  newWithdrawElement.value = "";
+
+  if (isNaN(newWithdrawTotal)) {
+    alert("Please Enter valid ammount");
+    return;
+  }
+
+  //previouswithdraw
+  const previousWithdrawElement = document.getElementById("withdraw");
+  const previousWithdrawTotalString = previousWithdrawElement.innerText;
+  const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
+  //balance
+  const previousBalanceElement = document.getElementById("balance");
+  const previousBalanceTotalSring = previousBalanceElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceTotalSring);
+
+  if (previousBalanceTotal < newWithdrawTotal) {
+    alert("insufecent balance");
+    return;
+  }
+  const currentWithdrawTotal = previousWithdrawTotal + newWithdrawTotal;
+  previousWithdrawElement.innerText = currentWithdrawTotal;
+
+  const newBalanceTotal = previousBalanceTotal - newWithdrawTotal;
+  previousBalanceElement.innerText = newBalanceTotal;
+});
+document.getElementById("logut").addEventListener("click", function () {
+  window.location.href = "index.html";
 });
